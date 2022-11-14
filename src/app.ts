139802ea7769e2dd080e5af48c0389739596510d
Scott Herold <sherold@mail.usf.edu>
2022-11-14 16:10:57 -0500

    (SMA-7) Added koa architecture to the application.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 'use strict'
 import koa from 'koa';
+import * as KoaRouter from 'koa-router';
+import koaBody from 'koa-body';
 
+// Need to require the koa-router
+const Router = require('koa-router');
+
+// Launch Server
 const app: koa = new koa();
+const router: KoaRouter = new Router();
+app.use(koaBody({ multipart: true }));
+app.use(router.routes());
 
+// PORT LISTENING
 app.listen(5000, 'localhost', () => {
     console.log('App running on localhost port 5000!')
 });
