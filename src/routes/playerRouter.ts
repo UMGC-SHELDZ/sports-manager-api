39a9a8cb7b1b8ddee983934984c5834718c62e8e
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2022-11-17 16:02:41 -0500

    getPlayer, getAllPlayers, updatePlayer, and deletePlayer added to routes and controllers.

--- src/routes/playerRouter.ts
+++ src/routes/playerRouter.ts
@@ -14,4 +14,28 @@ playerRouter.post('add-player',
     playerController.addPlayer
 );
 
+// GET route to get a single player
+playerRouter.get('get-player',
+    baseRoute,
+    playerController.getPlayer
+);
+
+// GET route to get all players
+playerRouter.get('get-all-players',
+    baseRoute,
+    playerController.getAllPlayers
+);
+
+// PATCH route to update a player by ID
+playerRouter.patch('update-player',
+    baseRoute,
+    playerController.updatePlayer
+);
+
+// DELETE route to delete a player
+playerRouter.delete('delete-player',
+    baseRoute,
+    playerController.deletePlayer
+);
+
 export default playerRouter;
