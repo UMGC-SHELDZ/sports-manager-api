083a1037b62c9906d7c70cd75c4325dcaf59a2f2
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2022-12-02 16:04:33 -0500

    Attempting to cast the incoming IDs into the controllers as Mongoose IDs

--- src/common/interfaces/models/team/ITeam.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-
-// Interface for the Team Mongoose model
-export default interface ITeam extends Document {
-    teamName: string;
-    managerName: string;
-    numPlayers: number;
-}
