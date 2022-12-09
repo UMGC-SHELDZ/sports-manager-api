80a1f377bced78b4aab3015ae857441d30b2ced1
Scott Herold <sherold@mail.usf.edu>
2022-12-08 19:58:49 -0500

    (SMA-35) JWT middlware added.

---
+++ src/middlware/authenticateToken.ts
@@ -0,0 +1,27 @@
+import { RouterContext } from "koa-router";
+import jwt from 'jsonwebtoken';
+
+// The secret key (must match in signature in login route)
+const secret = process.env.JWT_SECRET || 'jwt-secret';
+
+/**
+ * RMiddleware to authenticate a JWT token.
+ * @param {RouterContext} ctx The request context object.
+ * @param next The next request.
+ */
+const authenticateToken = async (ctx: RouterContext, next: () => Promise<void>): Promise<void> => {
+    console.log(ctx.headers.authorization);
+    if (!ctx.headers.authorization) ctx.throw(403, 'Access forbidden.');
+
+    const token = ctx.headers.authorization.split(' ')[1];
+
+    try {
+        ctx.request.body.jwtToken = jwt.verify(token, secret);
+    } catch (e: any) {
+        ctx.throw(e.status || 403, e.text);
+    }
+    
+    await next();
+};
+
+export default authenticateToken;
